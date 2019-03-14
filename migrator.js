c09331812b06c100732d04a9d1e52f18895d2934
Christopher J. Wright <cjwright4242@gmail.com>
2019-03-14 14:12:40 -0400

    inclue libprodobuf migration

--- migrator.js
+++ migrator.js
@@ -218,7 +218,9 @@ function createMigratorContainer(migratorName, parentId){
 }
 
 var migrators = [{name : "archrebuild", description : "ARM/PowerPC Migration Status" },
-{name : "openssl", description : "OpenSSL Migration Status" }];
+{name : "openssl", description : "OpenSSL Migration Status" },
+{name : "libprotobuf", description : "Libprotobuf 3.7 Miration Status"},
+];
 
 for (var migrator_key in migrators) {
     var migrator = migrators[migrator_key];
