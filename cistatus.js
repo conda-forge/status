d897a7bf87b7ec77983a5e8ae1d8dfa0c314c4fe
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-01-31 22:20:52 -0600

    ENH make the status live! (#66)
    * ENH make the status live!
    
    * added github
    
    * add links
    
    * add docs
    
    * links and subsection
    
    * reorg the page
    
    * reorg
    
    * try this
    
    * remove mostly
    
    * missed one
    
    * put it all back
    
    * rework to get migrations closer to the top

---
+++ cistatus.js
@@ -0,0 +1,39 @@
+
+function loadCIHTML (url, service, slug, callback) {
+  var xobj = new XMLHttpRequest()
+  // xobj.overrideMimeType('application/json')
+  xobj.open('GET', url, true)
+  xobj.responseType = 'document'
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      // Required use of an anonymous callback as .open will NOT return a value
+      // but simply returns undefined in asynchronous mode
+      callback(service, slug, xobj.response, url)
+    }
+  }
+  xobj.send(null)
+}
+
+function displayStatus (service, slug, htmlDocument, url) {
+  var div = document.getElementById('ciStatusDiv-' + slug)
+  div.innerHTML = ''
+  var p = document.createElement('h5')
+  var s = document.createElement('span')
+  div.appendChild(p)
+  p.innerHTML = '<a href="' + url + '">' + service + ' status</a>' + ': '
+  p.appendChild(s)
+
+  var stat = htmlDocument.getElementsByClassName('status')[0].getElementsByClassName('font-large')[0].innerHTML
+  stat = stat.trim()
+  s.innerHTML = stat
+  if (stat.toLowerCase() === 'all systems operational') {
+    s.className = 'status operational'
+  } else {
+    s.className = 'status degraded performance'
+  }
+}
+
+loadCIHTML('https://www.githubstatus.com/', 'GitHub', 'github', displayStatus)
+loadCIHTML('https://www.traviscistatus.com', 'Travis CI', 'travis', displayStatus)
+loadCIHTML('https://status.circleci.com', 'CircleCI', 'circle', displayStatus)
+loadCIHTML('https://status.appveyor.com', 'AppVeyor', 'appveyor', displayStatus)
