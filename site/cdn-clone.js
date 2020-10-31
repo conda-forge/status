2993c13133f3f6e180a2f7b905566db84f875de2
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-10-31 07:39:21 -0500

    ENH show the time (#95)

--- site/cdn-clone.js
+++ site/cdn-clone.js
@@ -1,29 +1,28 @@
-function loadTXT(url, callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.open('GET', url, true);
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
-
-function updateStatus(last_updated) {
-  var now_date = new Date();
-  var last_updated_date = new Date(last_updated.trim())
-  var THIRTY_MIN = 30 * 60 * 1000; /* ms */
-  var cloning_status = document.getElementsByClassName("cloning")[0].getElementsByClassName("cdn-status")[0]
-  if ((now_date.getTime() - last_updated_date.getTime()) < THIRTY_MIN){
-    cloning_status.className = "status operational"
-    cloning_status.innerHTML = "operational"
-  } else {
-    cloning_status.className = "status degraded performance"
-    cloning_status.innerHTML = "degraded"
+function loadTXT (url, callback) {
+  var xobj = new XMLHttpRequest()
+  xobj.open('GET', url, true)
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      callback(xobj.responseText)
+    }
   }
-
+  xobj.send(null)
 }
 
-var url = "https://s3.amazonaws.com/conda-static.anaconda.org/conda-forge/last-updated"
+function updateStatus (lastUpdated) {
+  var nowDate = new Date()
+  var lastUpdatedDate = new Date(lastUpdated.trim())
+  var THIRTY_MIN = 30 * 60 * 1000 /* ms */
+  var cloningStatus = document.getElementsByClassName('cloning')[0].getElementsByClassName('cdn-status')[0]
+  var cdnTimeMin = Math.round((nowDate.getTime() - lastUpdatedDate.getTime()) / 60.0 / 1000.0)
+  if ((nowDate.getTime() - lastUpdatedDate.getTime()) < THIRTY_MIN) {
+    cloningStatus.className = 'status operational'
+    cloningStatus.innerHTML = 'operational' + ' (cloned ' + cdnTimeMin + ' minutes ago)'
+  } else {
+    cloningStatus.className = 'status degraded performance'
+    cloningStatus.innerHTML = 'degraded' + ' (cloned ' + cdnTimeMin + ' minutes ago)'
+  }
+}
+
+var url = 'https://s3.amazonaws.com/conda-static.anaconda.org/conda-forge/last-updated'
 loadTXT(url, updateStatus)
