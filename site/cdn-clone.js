382db1c683e45ae7f1f4c8e87001d2ff78917763
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-12-15 06:29:30 -0600

    ENH redo  CDN status

--- site/cdn-clone.js
+++ site/cdn-clone.js
@@ -12,15 +12,19 @@ function loadTXT (url, callback) {
 function updateStatus (lastUpdated) {
   var nowDate = new Date()
   var lastUpdatedDate = new Date(lastUpdated.trim())
-  var THIRTY_MIN = 30 * 60 * 1000 /* ms */
+  var OK_MIN = 90 * 60 * 1000 /* ms */
+  var DEG_MIN = 120 * 60 * 1000 /* ms */
   var cloningStatus = document.getElementsByClassName('cloning')[0].getElementsByClassName('cdn-status')[0]
   var cdnTimeMin = Math.round((nowDate.getTime() - lastUpdatedDate.getTime()) / 60.0 / 1000.0)
-  if ((nowDate.getTime() - lastUpdatedDate.getTime()) < THIRTY_MIN) {
+  if ((nowDate.getTime() - lastUpdatedDate.getTime()) < OK_MIN) {
     cloningStatus.className = 'status operational'
-    cloningStatus.innerHTML = 'operational'
-  } else {
+    cloningStatus.innerHTML = 'operational' + ' (last cloned ' + cdnTimeMin + ' minutes ago)'
+  } else if ((nowDate.getTime() - lastUpdatedDate.getTime()) < DEG_MIN) {
     cloningStatus.className = 'status degraded performance'
     cloningStatus.innerHTML = 'degraded' + ' (last cloned ' + cdnTimeMin + ' minutes ago)'
+  } else {
+    cloningStatus.className = 'status major outage'
+    cloningStatus.innerHTML = 'major outage' + ' (last cloned ' + cdnTimeMin + ' minutes ago)'
   }
 }
 
