7e511c339d5384a0e14d694d8fdbe9ce40a53488
jaimergp <jaimergp@users.noreply.github.com>
2024-03-11 21:13:57 +0100

    Remove site and workflows, update README

--- site/repostatus.js
+++
@@ -1,6 +0,0 @@
-function statusImageNotFound (_this) {
-  var s = document.createElement('span')
-  s.innerHTML = 'No Status Available'
-  _this.parentNode.parentNode.appendChild(s)
-  _this.parentNode.parentNode.removeChild(_this.parentNode)
-}
